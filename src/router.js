fe62895c8f41e01498f5fa0d8484b3fd25f412ce
jacobhsu <jacob.hsu.tw@gmail.com>
2019-06-12 12:03:04 +0800

    login

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from './views/index.vue'
+import Index from './views/Index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -12,6 +12,22 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: ()=> import('./views/Login.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) =>{
+  const isLogin = localStorage.ele_login ? true : false;
+  if (to.path == '/login') {
+    next();
+  } else {
+    isLogin ? next() : next("/login");
+  }
+})
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+      <div class="logo"> 
+        <img src='../assets/logo.png' alt="login image">
+      </div>
+    login
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style>
+.login  {
+    width: 100%;
+    height: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+    background:#fff;
+}
+.logo {
+  text-align: center;
+}
+.logo img {
+  width: 150px;
+}
+</style>
